27dff673c755beff975a9831edb207702bc0577f
learnDD <42337500+learnDD@users.noreply.github.com>
2023-05-25 10:11:48 +0700

    add name to input field, separate function to external js, and create insert to db function for quotation (on progress)

--- quotation/pages/quotation.py
+++ quotation/pages/quotation.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def Quotation(request):
   
@@ -6,3 +6,36 @@ def Quotation(request):
     QuotationId = request.POST.get('QuotationId')
 
   return render(request, 'quotation.html')
+
+def insertQuotation(request):
+
+ 
+  Customer_ID = request.POST['Customer_ID']
+  Product_ID = request.POST['Product_ID']
+  Quantity = request.POST['quantity']
+  BudgetPerUnit = request.POST['BudgetPerUnit']
+  CostExcludeOperation = request.POST['CostExcludeOperation']
+  OperationCost = request.POST['OperationCost']
+  ManagementCostPercentage = request.POST['ManagementCostPercentage']
+  MaterialCostNumber = request.POST['MaterialCostNumber']
+  MaterialCostPercentage = request.POST['MaterialCostPercentage']
+  OutsorceCostNumber = request.POST['OutsorceCostNumber']
+  OutsorceCostPercentage = request.POST['OutsorceCostPercentage']
+  OperationBudget = request.POST['OperationBudget']
+  
+
+  query = 'Insert Into Quotation values ( , "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "")'.format(
+    Customer_ID,
+    Product_ID,
+    Quantity,
+    BudgetPerUnit,
+    CostExcludeOperation,
+    OperationCost,
+    ManagementCostPercentage,
+    MaterialCostNumber,
+    MaterialCostPercentage,
+    OutsorceCostNumber,
+    OutsorceCostPercentage,
+    OperationBudget,
+  )
+  return redirect('/quotation')
