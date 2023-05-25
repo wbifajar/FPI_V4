da5ae59c216c2207a81b33a2d4571a76744f4276
learnDD <42337500+learnDD@users.noreply.github.com>
2023-05-25 13:47:00 +0700

    fix delete process and add testing input database

--- quotation/pages/quotation.py
+++ quotation/pages/quotation.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.db import connection
+from ..databaseConnect import *
 
 def Quotation(request):
   
@@ -10,32 +12,51 @@ def Quotation(request):
 def insertQuotation(request):
 
  
-  Customer_ID = request.POST['Customer_ID']
-  Product_ID = request.POST['Product_ID']
-  Quantity = request.POST['quantity']
-  BudgetPerUnit = request.POST['BudgetPerUnit']
-  CostExcludeOperation = request.POST['CostExcludeOperation']
-  OperationCost = request.POST['OperationCost']
-  ManagementCostPercentage = request.POST['ManagementCostPercentage']
-  MaterialCostNumber = request.POST['MaterialCostNumber']
-  MaterialCostPercentage = request.POST['MaterialCostPercentage']
-  OutsorceCostNumber = request.POST['OutsorceCostNumber']
-  OutsorceCostPercentage = request.POST['OutsorceCostPercentage']
-  OperationBudget = request.POST['OperationBudget']
+  # Customer_ID = 2
+  # Product_ID = 2
+  Quantity = request.POST.get('quantity', False)  
+  # BudgetPerUnit = request.POST['BudgetPerUnit']
+  # CostExcludeOperation = request.POST['CostExcludeOperation']
+  # OperationCost = request.POST['OperationCost']
+  # ManagementCostPercentage = request.POST['ManagementCostPercentage']
+  # MaterialCostNumber = request.POST['MaterialCostNumber']
+  # MaterialCostPercentage = request.POST['MaterialCostPercentage']
+  # OutsorceCostNumber = request.POST['OutsorceCostNumber']
+  # OutsorceCostPercentage = request.POST['OutsorceCostPercentage']
+  # OperationBudget = request.POST['OperationBudget']
   
 
-  query = 'Insert Into Quotation values ( , "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "")'.format(
-    Customer_ID,
-    Product_ID,
-    Quantity,
-    BudgetPerUnit,
-    CostExcludeOperation,
-    OperationCost,
-    ManagementCostPercentage,
-    MaterialCostNumber,
-    MaterialCostPercentage,
-    OutsorceCostNumber,
-    OutsorceCostPercentage,
-    OperationBudget,
+  # query = 'Insert Into Quotation values ( , "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", )'.format(
+  #   Customer_ID,
+  #   Product_ID,
+  #   Quantity,
+  #   BudgetPerUnit,
+  #   CostExcludeOperation,
+  #   OperationCost,
+  #   ManagementCostPercentage,
+  #   MaterialCostNumber,
+  #   MaterialCostPercentage,
+  #   OutsorceCostNumber,
+  #   OutsorceCostPercentage,
+  #   OperationBudget,
+  # )
+
+  
+  query = 'Insert Into Quotation values ( NULL, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, CURRENT_TIMESTAMP)'.format(
+    1,
+    4,
+    4,
+    100,
+    12,
+    43,
+    2,
+    12,
+    2,
+    43,
+    12,
+    1,
   )
-  return redirect('/quotation')
+
+  with connection.cursor() as cursor:
+    cursor.execute(query)
+  return redirect('/Quotation/')
