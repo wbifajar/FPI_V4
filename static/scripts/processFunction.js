b06e21366303d5d28cf27429387944ff2cf0307b
claratanudy <clara.535200025.stu.untar.ac.id>
2023-08-01 10:43:39 +0700

    detail other

--- static/scripts/processFunction.js
+++ static/scripts/processFunction.js
@@ -153,7 +153,7 @@ function addProcess() {
 	cell2.style.width = "5%";
 
 	cell1.innerHTML = `<th scope="row">${index}</th>`; 
-	cell2.innerHTML = `<td><input name='ProcessId' value='${selectedProcess.ProcessId}' style="background: transparent; border: none; width: 100%;></td>`;  
+	cell2.innerHTML = `<td><input name='ProcessId' value='${selectedProcess.ProcessId}' style="background: transparent; border: none; width: 100%; disabled></td>`;  
 	cell3.innerHTML = `<td>${selectedProcess.Name}</td>`;
 	cell4.innerHTML = `<td>${selectedProcess.SettingCost}/${selectedProcess.ProcessCost}</td>`;
 	cell5.innerHTML = `<td><input type="text" class="operationInputMd" onfocus="savePrevValue()" onchange="calculateByOpeSum(${index})" name='opeSum' id='opeSum-${index}' value='0'></td>`;
