d33ed5536d6cfd09eac4f4be7397c27521a1d149
learnDD <42337500+learnDD@users.noreply.github.com>
2023-09-15 01:06:28 +0700

    update update

--- quotation/pages/quotation.py
+++ quotation/pages/quotation.py
@@ -20,13 +20,12 @@ def Quotation(request):
 
     # Modify and calculate values for each quotation
     for item in quotation:
-        duration = item['DURATION']
         item['QUANTITY'] = int(item['QUANTITY'])
         item['BUDGET_PER_UNIT'] = int(item['BUDGET_PER_UNIT'])
         item['TOTAL'] = int(item['QUANTITY']) * int(item['BUDGET_PER_UNIT'])
-        item['EXPIRED'] = (item['CREATED_AT']) + timedelta(days=duration)
         item['QUOTATION_ID'] = item['QUOTATION_ID']
         item['QUOTATION_STATUS'] = item['QUOTATION_STATUS']
+        item['expired_date'] = str(item['expired_date'])
 
 
     # Extract only the required fields for JSON serialization
@@ -325,33 +324,34 @@ def insertQuotation(request):
         NumberQuotationatthatday = get_next_quotation_number(today)
         QuotationNo = f"{today}-{NumberQuotationatthatday:04}"
 
-        Duration = request.POST.get('quotation_duration', False)
+        ExpiredDate = request.POST.get('expired_date', False)
         is_active = 1
+        
         print("STATUS = ", Status)
 
-        query = 'INSERT INTO Quotation VALUES ( null, "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}")'.format(
-            CustomerID,
-            ProductID,
-            ProductName,
-            ProductVersion,
-            Quantity,
-            BudgetPerUnit,
-            CostExcludeOperation,
-            OperationCost,
-            ManagementCostPercentage,
-            MaterialCostNumber,
-            MaterialCostPercentage,
-            OutsourceCostNumber,
-            OutsourceCostPercentage,
-            OperationBudget,
-            timezone.now(),
-            Username,
-            Status,
-            Duration,
-            QuotationNo
-            is_active
-        )
-
+        query = f'INSERT INTO Quotation VALUES ( null, \
+                "{CustomerID}", \
+                "{ProductID}", \
+                "{ProductName}", \
+                "{ProductVersion}", \
+                "{Quantity}", \
+                "{BudgetPerUnit}", \
+                "{CostExcludeOperation}", \
+                "{OperationCost}", \
+                "{ManagementCostPercentage}", \
+                "{MaterialCostNumber}", \
+                "{MaterialCostPercentage}", \
+                "{OutsourceCostNumber}", \
+                "{OutsourceCostPercentage}", \
+                "{OperationBudget}", \
+                "{timezone.now()}", \
+                "{Username}", \
+                "{ Status,}", \
+                "{is_active}", \
+                "{QuotationNo}", \
+                "{ExpiredDate}" )'
+        # return HttpResponse(query)
+        print(query)
         with connection.cursor() as cursor:
             cursor.execute(query)
         
@@ -392,6 +392,7 @@ def detailQuotation(request, quotation_id):
         totalBudget = quotation["QUANTITY"] * quotation["BUDGET_PER_UNIT"]
         totalCost = quotation["COST_EXCLUDE_OPERATION"] + quotation["OPERATION_COST"]
         managementCost = quotation["BUDGET_PER_UNIT"] * 0.3
+        quotation['expired_date'] = str(quotation['expired_date'])
         # totalMaterialCost =  quotation["MATERIAL_COST_NUMBER"] * ( quotation["MATERIAL_COST_PERCENTAGE"] / 100 )
         # totalOutsourceCost = quotation["OUTSOURCE_COST_NUMBER"] * ( quotation["OUTSOURCE_COST_PERCENTAGE"] / 100 )
         
@@ -452,6 +453,7 @@ def detailQuotation(request, quotation_id):
         quotation_other_js = json.dumps(quotation_other)
         # print("quotation other = ", quotation_other)
 
+        print("quotation = ", quotation)
         # context
         context = { 
             'q' : quotation,
@@ -712,7 +714,8 @@ def updateQuotation(request, quotation_id):
         OperationBudget = request.POST['OperationBudget'].replace(',', '')
         Username = request.user.username
         Status = request.POST.get('quotation_status', False)
-        Duration = request.POST.get('quotation_duration', False)
+        ExpiredDate = request.POST.get('expired_date', False)
+        
         is_active = 1
         print("STATUS = ", Status)
 
@@ -733,7 +736,7 @@ def updateQuotation(request, quotation_id):
                 CREATED_AT = "{  timezone.now() }", \
                 ACTIVITY_LOG = "{ Username }", \
                 QUOTATION_STATUS = "{ Status }", \
-                DURATION = { Duration }, \
+                expired_date = "{ExpiredDate }", \
                 is_active = {is_active} \
                 WHERE QUOTATION_ID = { quotation_id }'
         
@@ -749,6 +752,11 @@ def updateQuotation(request, quotation_id):
     
 
 def copyQuotation(request,  quotation_id):
+
+    today = timezone.now().strftime('%Y%m%d')
+    NumberQuotationatthatday = get_next_quotation_number(today)
+    QuotationNo = f"{today}-{NumberQuotationatthatday:04}"
+    print('-- QUOTATION_NO = ', QuotationNo)
     query = f"INSERT INTO QUOTATION ( \
             SELECT  \
             null, \
@@ -769,10 +777,12 @@ def copyQuotation(request,  quotation_id):
             CREATED_AT, \
             ACTIVITY_LOG, \
             QUOTATION_STATUS, \
-            DURATION, \
-            is_active \
+            is_active, \
+            '{QuotationNo}', \
+            EXPIRED_DATE \
             FROM quotation \
             WHERE QUOTATION_ID = {quotation_id});"
+    print("-- query copy quotaion = ", query)
     with connection.cursor() as cursor:
         cursor.execute(query)
         q = cursor.fetchall()
