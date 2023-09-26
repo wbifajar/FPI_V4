63b63de70057f2c3e34003a6bfb29bbcf96bb67a
bertrandoey <bertrandoey05@gmail.com>
2023-09-26 11:05:25 +0700

    page part

--- part/urls.py
+++ part/urls.py
@@ -4,5 +4,10 @@ from . import views
 
 app_name = "part"
 urlpatterns = [
-    path("", views.part, name="index"),
+    path("", views.index, name="index"),
+    path("insert", views.insert, name="insert"),
+    path("store", views.store, name="store"),
+    path("edit/<int:idPart>", views.edit, name="edit"),
+    path("update/<int:idPart>", views.update, name="update"),
+    path("delete/<int:idPart>", views.delete, name="delete"),
 ]
